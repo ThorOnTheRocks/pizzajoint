ae346f52b641642e73adf333d9fed49439891c71
gianluca galota <gianluca.galota@gmail.com>
2022-04-27 22:54:37 +0100

    created draggable svg logo

---
+++ src/components/Order.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion'
+
+const orderContainerVariants = {
+  hidden: {
+    x: '100vw',
+    opacity: 0,
+  },
+
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      type: 'spring',
+      mass: 0.4,
+      damping: 8,
+      when: "beforeChildren",
+      staggerChildren: 0.4
+    }
+  },
+
+  exit: {
+    x: '-100vw',
+    transition: {
+      ease: 'easeInOut'
+    }
+  }
+}
+
+const childVariants = {
+  hidden: {
+    opacity: 0
+  },
+  visible: {
+    opacity: 1
+  }
+}
+
+const Order = ({ pizza, setShowModal }) => {
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowModal(true);
+    }, 5000)
+  }, [setShowModal])
+
+  return (
+    <motion.div
+      className="container order"
+      variants={orderContainerVariants}
+      initial="hidden"
+      animate="visible"
+      exit="exit"
+    >
+      <h2>Thank you for your order :)</h2>
+      <motion.p
+        variants={childVariants}
+
+      >
+        You ordered a {pizza.base} pizza with:
+      </motion.p>
+      <motion.div
+        variants={childVariants}
+      >
+        {pizza.toppings.map(topping => <div key={topping}>{topping}</div>)}
+      </motion.div>
+    </motion.div>
+  )
+}
+
+export default Order;
---
+++ src/components/Toppings.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+
+const toppingsContainerVariants = {
+  hidden: {
+    x: '100vw',
+    opacity: 0,
+  },
+
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      type: 'spring',
+      delay: 0.5
+    }
+  },
+
+  exit: {
+    x: '-100vw',
+    transition: {
+      ease: 'easeInOut'
+    }
+  }
+}
+
+const buttonVariants = {
+  hover: {
+    scale: 1.1,
+    textShadow: "0 0 8px rgba(255, 255, 255)",
+    boxShadow: "0 0 8px rgba(255, 255, 255)",
+    transition: {
+      duration: 0.3,
+      yoyo: Infinity,
+    }
+  }
+}
+
+const Toppings = ({ addTopping, pizza }) => {
+  let toppings = ['mushrooms', 'peppers', 'onions', 'olives', 'extra cheese', 'tomatoes'];
+
+  return (
+    <motion.div
+      className="toppings container"
+      variants={toppingsContainerVariants}
+      initial="hidden"
+      animate="visible"
+      exit="exit"
+    >
+
+      <h3>Step 2: Choose Toppings</h3>
+      <ul>
+        {toppings.map(topping => {
+          let spanClass = pizza.toppings.includes(topping) ? 'active' : '';
+          return (
+            <motion.li
+              key={topping}
+              onClick={() => addTopping(topping)}
+              whileHover={{ scale: 1.3, originX: 0, color: '#f8e112' }}
+              transition={{ type: 'spring', stiffness: 300 }}
+            >
+              <span className={spanClass}>{topping}</span>
+            </motion.li>
+          )
+        })}
+      </ul>
+
+      <Link to="/order">
+        <motion.button
+          variants={buttonVariants}
+          whileHover="hover"
+        >
+          Order
+        </motion.button>
+      </Link>
+
+    </motion.div>
+  )
+}
+
+export default Toppings;
